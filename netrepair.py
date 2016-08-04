30276e7154f3f4d6b28cfa11c6db296c5ca4966c
Kevin Kahn <kevin@coral-zone.com>
2016-08-03 17:26:09 -0700

    Prelim version

--- netrepair.py
+++ netrepair.py
@@ -3,13 +3,15 @@ import os
 import time
 from datetime import timedelta
 
-def updatestateandfilestamp(ns,rs):
-	global netstate, recoverystate, statusfile
+
+def updatestateandfilestamp(state):
+	global recoverystate, statusfile
 	with open(statusfile,'w',0) as f:
-		f.writelines(ns)
-		f.writelines(rs)
-	netstate = ns
-	recoverystate = rs
+		f.write(state)
+	logit('Recovery state changed - was: ' + recoverystate + ' now: ' + state)
+	recoverystate = state
+
+
 
 def touch():
 	with open(statusfile,'a'):
@@ -18,23 +20,24 @@ def touch():
 def logit(msg):
 	global logfile, netstate, recoverystate
 	with open(logfile,'a',0) as f:
-		f.write(time.strftime('%a %d %b %Y %H:%M:%S ')+msg+' '+netstate+' '+recoverystate+'\n')
+		f.write(time.strftime('%a %d %b %Y %H:%M:%S ') + msg + ' (' + recoverystate + ')\n')
 
 def getuptime():
 	with open('/proc/uptime', 'r') as f:
 		up_seconds = float(f.readline().split()[0])
-	up_string = str(timedelta(seconds=uptime_seconds))
+	up_string = str(timedelta(seconds=up_seconds))
 	return up_seconds, up_string
 
 router = '192.168.1.1'
 google = '8.8.8.8'
+# google = '192.168.3.3'
 statusfile = '/home/pi/watchdog/lastcheck'
 logfile = '/home/pi/watchdog/watchdog.log'
 
 uptimebeforechecks = 10 # seconds to wait after reboot for pi network to stabilize
 
-basecycletime = 5 # seconds to sleep per cycle
-glitchtime = 5 # seconds to wait for net to spontaneously fix itself
+basecycletime = 30  # seconds to sleep per cycle
+glitchtime = 60  # seconds to wait for net to spontaneously fix itself
 modemtimelimit = 4 * 60  # this times basecycletime is how long to wait for modem to come up
 routertimelimit = 2 * 60  # this times basecycletime is how long to wait for router to come up
 ISPtimelimit = 60 * 60 # delay when it looks like ISP outage
@@ -43,20 +46,23 @@ modemcount = 0
 routercount = 0
 ISPcount = 0
 
+recoverystate = '-------'
+logit("Startup")
+
 # on startup get the status from the previous run/boot cycle or initialize if missing
 
 if os.path.isfile(statusfile):
 	with open(statusfile, 'r', 0) as g:
-		netstate = g.readline()
 		recoverystate = g.readline()
 else:
-	updatestateandfilestamp('unknown','watching')
+	updatestateandfilestamp('unknown')
 
-logit("Startup")
+
 logit("Up: "+getuptime()[1])
 
 while True:
 	# should set a reasonably short t/o on these to avoid setting the cycle too far off
+	cyclestart = time.time()
 	inetup = pyping.ping(google)
 	routerup = pyping.ping(router)
 
@@ -71,12 +77,16 @@ while True:
 
 	if inetup.ret_code == 0:
 		# All is well
-		if netstate <> 'netup':
+		if recoverystate <> 'watching':
 			# just came up so log it
-			logit('Network is up - was: ' + netstate + '/' + recoverystate + 'modemcnt: ' + str(modemcount) + 'routercnt: ' + str(routercount))
-		updatestateandfilestamp('netup','watching')
+			logit('Network is up - was: ' + recoverystate + ' modemcnt: ' + str(modemcount) + ' routercnt: ' + str(
+				routercount))
+			updatestateandfilestamp('watching')
+		else:
+			touch()
+		netlastseen = cyclestart  # note last seen ok time to allow for glitches
 
-	elif routerup.ret_code == 0:
+	elif (routerup.ret_code == 0) and (cyclestart > netlastseen + glitchtime):
 		# net down router up - modem down or ISP down
 		if recoverystate == 'rebootingmodem':
 			# already started the reboot so just count cycles - do need to write watched file though
@@ -84,36 +94,36 @@ while True:
 			touch()
 			logit('Waiting on modem: '+str(modemcount))
 			if modemcount > modemtimelimit:
-				logit('Probably ISP outage ')
+				logit('Probable ISP outage ')
 				ISPcount = 0
-				updatestateandfilestamp('onlylocal','ISPoutage')
+				updatestateandfilestamp('ISPoutage')
 		elif recoverystate == 'ISPoutage':
 			ISPcount += basecycletime
 			touch()
 			if ISPcount > ISPtimelimit:
-				updatestateandfilestamp('onlylocal','TotalReset')
+				updatestateandfilestamp('TotalReset')
 				logit('Long ISP outage - trying a total reset')
 				# power cycle modem and router and Pi todo
 				print "Reboot all - ISP"
 				# exit??
 		else:
 			# not already doing anything about the outage - reboot modem (should we wait a cycle or 2?
-			# update file to local, restartingmodem
-			updatestateandfilestamp('onlylocal','rebootingmodem')
+			updatestateandfilestamp('rebootingmodem')
 			modemcount = 0
 			logit('Start modem reboot')
 			print "Reboot modem"
 
 			# cycle the power on modem todo
-	else:
+	elif cyclestart > netlastseen + glitchtime:  # allow for short net outages
 		# No router response and can't tell about modem of course
+		# Could reboot router first but Pi is more likely to have failed to try it first
 		if recoverystate == 'watching':
-			# reboot the pi - do this here with a sys reboot call or let watchdog do it?
-			# set recoverystate to "rebootedpi" so we know after restart todo
+			updatestateandfilestamp('rebootedpi')  # set recoverystate to "rebootedpi" so we know after restart
 			print "Reboot pi"
+		# todo reboot the pi end exit this program so the watchdog goes off if the reboot doesn't happen
 		elif recoverystate == 'rebootedpi':
 			# just rebooted pi but didn't get the network so reboot the router
-			updatestateandfilestamp('nonet', 'rebootingrouter')
+			updatestateandfilestamp('rebootingrouter')
 			routercount = 0
 			logit('Start router reboot')
 
@@ -124,19 +134,20 @@ while True:
 		elif recoverystate == 'rebootingrouter':
 			# have started the modem reboot so wait on that
 			routercount += basecycletime
-			updatestateandfilestamp('nonet','rebootingrouter')
+			touch()
 			logit("Waiting on router " + str(routercount))
 
 			# if routercount get too high?
 			#  routercount should get us about 90 seconds
 			# wait 10 minutes and then cycle everything modem then router then pi
 		else:
-			# huh?  or perhaps check for initial?
-			pass
+			logit('Unknown state error: ' + recoverystate)  # huh?  or perhaps check for initial?
+		# reboot system - something seriously wrong todo
+
+	else:
+		logit("Net lost - outage time: " + str(cyclestart - netlastseen))
+		touch()
 
 	# sleep awaiting events
-	time.sleep(basecycletime)
-
-
-
-
+	cycleend = time.time()
+	time.sleep(basecycletime - (cycleend - cyclestart))
