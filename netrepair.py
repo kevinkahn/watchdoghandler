75593f411ea4e940f05ac79a24943972626ed8de
Kevin Kahn <kevin@coral-zone.com>
2016-08-07 11:10:26 -0700

    more changes including a noaction option for the pi reboots

--- netrepair.py
+++ netrepair.py
@@ -1,13 +1,14 @@
 import os
 import sys
 import time
+import subprocess
 from datetime import timedelta
 
 import RPi.GPIO as GPIO
 import pyping
 
 
-class device(object):
+class Device(object):
 	def __init__(self, n, port, dt):
 		self.pin = port
 		self.resettime = time.time()  # initialize to current time to handle case where a reset op was in progress
@@ -31,11 +32,36 @@ class device(object):
 	# just won't wait as long as expected
 
 
+def pireboot(msg, ecode):
+	# cause the Pi to reboot - first gently, then violently, then via the hw timer
+	global noaction
+	logit(msg)
+	updatestateandfilestamp('rebootedpi')
+	if noaction:
+		logit('Supressed reboot')
+		print 'Supressed reboot'
+		return
+	subprocess.call('sudo reboot now', shell=True)
+	time.sleep(60)  # this should never finish
+	# why are we still running - drastic measures called for
+	logit("Violent reset " + msg)
+	with open('/proc/sys/kernel/sysrq') as s:
+		s.write('1')
+	with open('/proc/sysrq-trigger') as s:
+		s.write('b')
+	logit("Post violent kill " + msg)
+	print "PiReboot: " + str(ecode)
+	sys.exit(ecode)
+
+
+# last try is the watchdog time expiry
+
+
 def updatestateandfilestamp(state):
 	global recoverystate, statusfile
 	with open(statusfile,'w',0) as f:
 		f.write(state)
-	logit('Recovery state changed - was: ' + recoverystate + ' now: ' + state)
+	logit('State change: ' + recoverystate + ' to: ' + state)
 	recoverystate = state
 
 def touch():
@@ -60,28 +86,29 @@ def getuptime():
 	up_string = str(timedelta(seconds=up_seconds))
 	return up_seconds, up_string
 
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.cleanup()
 
+noaction = True
 routerIP = '192.168.1.1'
 googleIP = '8.8.8.8'
 
-modem = device('modem', 4, 1*60)
-router = device('router', 17, 4*60)
-ISP = device('ISP', 0, 60*60)
+modem = Device('modem', 22, 1*60)
+router = Device('router', 17, 4*60)
+ISP = Device('ISP', 0, 60*60)
 
 logskip = 0
-logskipstart = 10
+logskipstart = 30
 
 # google = '192.168.3.3'
 statusfile = '/home/pi/watchdog/lastcheck'
 logfile = '/home/pi/watchdog/watchdog.log'
 
 uptimebeforechecks = 10 # seconds to wait after reboot for pi network to stabilize
-
 basecycletime = 30  # seconds to sleep per cycle
+outagebeforepireset = 12*60*60  # 12 hours
+
 
 recoverystate = '-------'
 logit("Startup")
@@ -93,21 +120,22 @@ if os.path.isfile(statusfile):
 		recoverystate = g.readline()
 else:
 	updatestateandfilestamp('unknown')
-
+netlastseen = 0
 
 logit("Up: "+getuptime()[1])
 
 while True:
 	cyclestart = time.time()
-	inetup = pyping.ping(googleIP, timeout=100, count=2)
-	routerup = pyping.ping(routerIP)
-	print cyclestart, inetup.ret_code, routerup.ret_code
+	print cyclestart,
+	inetup = pyping.ping(googleIP, timeout=200, count=3)
+	routerup = pyping.ping(routerIP, timeout=200, count=3)
+	print time.time(), inetup.ret_code, routerup.ret_code
 	# print inetup.avg_rtt, routerup.avg_rtt
 	uptime_seconds, uptime_string = getuptime()
 
 	if uptime_seconds < uptimebeforechecks:
 		# Pi probably hasn't actually gotten the net up yet
-		logit('Waiting on minumum uptime: ' + uptime_string)
+		logit('Waiting minumum uptime: ' + uptime_string)
 		os.utime(statusfile,None)
 		time.sleep(1)
 		continue
@@ -116,12 +144,12 @@ while True:
 		# All is well - clear any reset in progress and just get back to watching
 		if recoverystate <> 'watching':
 			# just came up so log it
-			logit('Network is up - was: ' + recoverystate)
+			logit('Network up from: ' + recoverystate)
 			updatestateandfilestamp('watching')
 		else:
 			touch()
 			logit('Network ok', lowfreq=True)
-		netlastseen = cyclestart  # note last seen ok time to allow for glitches
+		netlastseen = cyclestart  # note last seen ok time
 
 	elif recoverystate == 'ISPfullreset1':
 		# doing a full reset so wait on modem time to reset then do modem reset
@@ -130,7 +158,7 @@ while True:
 			pass
 		else:
 			# waited long enough for modem to reset now try router
-			recoverystate = 'ISPfullreset2'
+			updatestateandfilestamp('ISPfullreset2')
 			router.reset()
 	elif recoverystate == 'ISPfullreset2':
 		# rebooting the router during a full reset
@@ -139,9 +167,14 @@ while True:
 			pass
 		else:
 			# modem and router now cycled and net still out so assume ISP down
-			logit('Full reset failed to restore network')
-			ISP.reset()
-			updatestateandfilestamp('ISPoutage')  # todo at some point should we reboot the Pi?
+			logit('Full reset failed to restore')
+			if time.time() > netlastseen + outagebeforepireset:
+				# long enough to shoot the Pi and restart stuff
+				pireboot("ISP outage limit", 99)
+			else:
+				# wait another ISP outage interval
+				ISP.reset()
+				updatestateandfilestamp('ISPoutage')
 
 	elif routerup.ret_code == 0:
 		# net down router up - modem down or ISP down
@@ -161,7 +194,7 @@ while True:
 				logit('ISP out', lowfreq=True)
 			else:
 				updatestateandfilestamp('ISPfullreset1')
-				logit('Long ISP outage - reset modem again')
+				logit('Long ISP outage - reset modem')
 				modem.reset()
 				print "ISP outage modem/router reset"
 		elif recoverystate == 'startmodemreboot':
@@ -173,14 +206,13 @@ while True:
 		elif recoverystate == 'watching':
 			# plan to do modem reboot if still down next cycle
 			updatestateandfilestamp('startmodemreboot')
-		else:
-			logit('Unknown state error 1: ' + recoverystate)  # huh?  or perhaps check for initial?
-			updatestateandfilestamp('unknown')
-			print "Reboot pi"
-			# subprocess.call('sudo reboot now', shell=True)
-			time.sleep(5)
-			sys.exit(2)
+		elif recoverystate == "picommsunknown":
+			# comms are really flakey - lost local then it came back but net out
+			# best to do a reset
+			pireboot("Wavering connections", 91)
 
+		else:
+			pireboot('Unknown state error 1: ' + recoverystate, 92)  # huh?  or perhaps check for initial?
 
 	else:
 		# no local ping response
@@ -205,19 +237,10 @@ while True:
 		elif recoverystate == 'picommsunknown':
 			# comms were down last cycle so start the reboots
 			# Could reboot router first but Pi is more likely to have failed to try it first
-			updatestateandfilestamp('rebootedpi')  # set recoverystate to "rebootedpi" so we know after restart
-			print "Reboot pi"
-			# subprocess.call('sudo reboot now', shell=True)
-			time.sleep(5)
-			sys.exit(1)
+			pireboot('No local comms', 91)
 
 		else:
-			logit('Unknown state error 2: ' + recoverystate)  # huh?  or perhaps check for initial?
-			updatestateandfilestamp('unknown')
-			print "Reboot pi"
-			# subprocess.call('sudo reboot now', shell=True)
-			time.sleep(5)
-			sys.exit(3)
+			pireboot('Unknown state error 2: ' + recoverystate, 93)  # huh?  or perhaps check for initial?
 
 	# sleep awaiting events
 	cycleend = time.time()
