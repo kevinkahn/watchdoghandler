6f518fd7efabbb551046fc8cb624ed3b86c20712
Kevin Kahn <kevin@coral-zone.com>
2016-10-13 10:07:26 -0700

    Fixing watchdog install

--- WDsetup.sh
+++ WDsetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Setup the watchdog stuff
 apt-get install -y watchdog
+pip install PyYAML
+pip install pyping
 cp support/watchdog.service /lib/systemd/system
 cp support/watchdog.conf /etc
 cp support/nethealth.service /etc/systemd/system
