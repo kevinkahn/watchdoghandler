ec9683ac3280c2e44608a93c0c0c7be13fdc3988
Kevin Kahn <kevin@coral-zone.com>
2016-10-13 14:05:20 -0700

    Fixing watchdog install

--- WDsetup.sh
+++ WDsetup.sh
@@ -6,6 +6,9 @@ pip install pyping
 cp support/watchdog.service /lib/systemd/system
 cp support/watchdog.conf /etc
 cp support/nethealth.service /etc/systemd/system
+chmod a+x repair.py
+chmod a+x netrepair.py
+echo dtparam=watchdog=on >> /boot/config.txt
 systemctl daemon-reload
 systemctl enable watchdog
 systemctl enable nethealth
