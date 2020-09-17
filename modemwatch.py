2bb3cdce83abaa6f48e0187a80ffe1f9c2a4f74c
Kevin Kahn <kevin@coral-zone.com>
2020-09-16 19:54:12 -0700

    Update to handle multiple devices to be reset and more flexible testing (separate simulation of net and resets)

--- modemwatch.py
+++ modemwatch.py
@@ -5,20 +5,19 @@ import requests
 import os
 import subprocess
 def logit(msg):
-	global logfile, test
+	global logfile
 
+#	with open(logfile,'a',0) as f:
 	logfile.write(time.strftime('%a %d %b %Y %H:%M:%S: ') + msg + '\n')
 	logfile.flush()
-	if test:
-		print(time.strftime('%a %d %b %Y %H:%M:%S: ') + msg)
 
 def RobustPing(dests, verbose=True):
-	global badping, testfile, destindex, lastdest, null, test
+	global badping, testfile, destindex, lastdest, null, simnet
 	dest = dests[destindex]
 	lastdest = dest
 	destindex = (destindex + 1)%len(dests)
 
-	if test: # for testing set destinations to ~L ~R
+	if simnet: # for testing set destinations to ~L ~R
 		if os.path.isfile(testfile):
 			logit("Simulate good ping of "+dest)
 			return True
@@ -26,9 +25,9 @@ def RobustPing(dests, verbose=True):
 			logit("Simulate failed ping of "+dest)
 			return False
 	ok = False
-	cmd = 'ping -c 1 -W 1 ' + dest
+	pingcmd = 'ping -c 1 -W 1 ' + dest
 	for i in range(10):
-		p = subprocess.call(cmd, shell=True, stdout=null, stderr=null)
+		p = subprocess.call(pingcmd, shell=True, stdout=null, stderr=null)
 		if p == 0:
 			ok = True # one success in loop is success
 			break
@@ -40,6 +39,24 @@ def RobustPing(dests, verbose=True):
 				if badping % 100 == 0:
 					logit('Ping failure count = '+str(badping))
 	return ok
+
+def IssueReset(cmdtoissue):
+	tries = 5
+	success = False
+	while not success:
+		try:
+			r = requests.get(cmdtoissue)
+			logit('Reset try response (tries = {}): {}'.format(tries, r.status_code))
+			if r.status_code == 200:
+				success = True
+		except Exception as E:
+			logit('Exception trying to do reset (tries  {}): {}'.format(tries, E))
+			tries = tries - 1
+			if tries > 0:
+				time.sleep(15)
+			else:
+				logit('Reset failed multiple times - restart watchdog')
+				raise ConnectionAbortedError
 
 badping = 0
 destindex = 0
@@ -56,19 +73,30 @@ parser.add_argument('-w', '--wait', default = 120, type = int, help = 'time to w
 parser.add_argument('--dests',nargs='*',default='8.8.8.8')
 parser.add_argument('--logfile',default = 'modemwatch.log', type=argparse.FileType('a'))
 parser.add_argument('-v', '--verbose',default = False, action='store_true')
-parser.add_argument('-t', '--test',default=False,action='store_true')
+parser.add_argument('--simreset',default=False,action='store_true')
+parser.add_argument('--simnet',default=False,action='store_true')
 parser.add_argument('-c', '--confirm',default=12, type = int, help = 'interval in hours for issuing log messages confirming running')
 parser.add_argument('--testfile',default='simnetup')
-parser.add_argument('--cmd', default='')
-parser.add_argument('--modem',default='modempowercontrol.pdxhome')
+parser.add_argument('--cmdm', default='')
+parser.add_argument('--cmdr', default='')
+parser.add_argument('--modem',default='modempower.pdxhome')
+parser.add_argument('--router',default='routerpower.pdxhome')
+parser.add_argument('--dualdevices',default=False,action='store_true')
+parser.add_argument('--interdelay', default=1,type=int,help = 'delay between modem reset and router reset')
 args = parser.parse_args()
 logfile = args.logfile
-if args.cmd == '':
-	cmd = 'http://'+args.modem+'/cm?cmnd=Power1%20ON'
+if args.cmdm == '':
+	cmdm = 'http://'+args.modem+'/cm?cmnd=Power1%20ON'
 else:
-	cmd = args.cmd
-test = args.test
-if test:
+	cmdm = args.cmdm
+if args.cmdr == '':
+	cmdr = 'http://'+args.router+'/cm?cmnd=Power1%20ON'
+else:
+	cmdr = args.cmdm
+
+simreset = args.simreset
+simnet = args.simnet
+if simnet:
 	args.interval = 1
 	args.outage = .25
 	args.wait = 10
@@ -108,26 +136,17 @@ while True:
 			now = time.time()
 			logit('Ping continues fail: '+ lastdest + ' Down {0:.0f} seconds {1:.0f} to reset'.format(now - netdowntime,args.outage*60 - (now-resettime)))
 			if now - resettime > args.outage*60:
-				if test:
-					logit('Test modem reset; delay {} seconds: {}'.format(args.wait, cmd))
+				if simreset:
+					logit('Test modem reset; delay {} seconds: {}'.format(args.wait, cmdm))
+					if args.dualdevices: logit('Test router reset; delay {} seconds: {}'.format(args.wait, cmdr))
 				else:
-					logit('Issue modem reset and delay {0:d} seconds'.format(args.wait))
-					tries = 5
-					success = False
-					while not success:
-						try:
-							r = requests.get(cmd)
-							logit('Reset try response (tries = {}): {}'.format(tries, r.status_code))
-							if r.status_code == 200:
-								success = True
-						except Exception as E:
-							logit('Exception trying to do reset (tries  {}): {}'.format(tries, E))
-							tries = tries - 1
-							if tries > 0:
-								time.sleep(15)
-							else:
-								logit('Reset failed multiple times - restart watchdog')
-								raise ConnectionAbortedError
+					logit('Issue modem reset')
+					IssueReset(cmdm)
+					if args.dualdevices:
+						time.sleep(args.interdelay)
+						logit('Issue router reset')
+						IssueReset(cmdr)
+					logit('Delay {0:d} seconds'.format(args.wait))
 
 
 				time.sleep(args.wait)
